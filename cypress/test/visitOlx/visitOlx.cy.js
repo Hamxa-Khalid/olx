874c805282750f23631a7d6945108a6f1f52447c
Hamxa-Khalid <hamxi8381@gmail.com>
2023-06-01 00:30:18 +0500

    Deals with alert

--- cypress/test/visitOlx/visitOlx.cy.js
+++ cypress/test/visitOlx/visitOlx.cy.js
@@ -1,11 +1,25 @@
-import { OlxHomePage } from "../../support/pages/vistOlxPO/visitOlxPO"
+import { OlxHomePage } from "../../support/pages/visitOlxPO/visitOlxPO"
 
 describe("Visit Olx Website", function () {
-    before(function(){
+    before(function () {
         cy.visit("/");
     })
 
     it("Validate Olx Home Page", function () {
-        const olxHomePage = new OlxHomePage;
+
+        cy.url().should('include', 'olx.com');
+        cy.title().should('eq', 'OLX - Buy and Sell for free anywhere in Pakistan with OLX online classifieds');
+
+        // cy.on('window:confirm', (t) => {
+        //     expect(t).to.contains("Don't Allow");
+        //     return false;
+        // })
+        
+        cy.get('button').contains("Don't Allow").click();
+        
+        // cy.get("input[type='search']").type('Mobiles');
+        // cy.get('button[aria-label="Search"]').click();
     })
 })
+
+
