c01dc103c150d81dd3318fa3d17d58a618f49f90
Hamxa-Khalid <hamxi8381@gmail.com>
2023-06-07 22:04:49 +0500

    Completed Login Test Case Without adding data to fixture, Created a custom command so alert handling code can be reuseable

--- cypress/support/pages/loginOlxPO/loginOlxPO.js
+++ cypress/support/pages/loginOlxPO/loginOlxPO.js
@@ -1,5 +1,25 @@
 export class loginOlx {
     getOlxLoginButton() {
-        return cy.get('[aria-label="Login"]');
+        return cy.get('[aria-label="Login"]').should('have.text','Login');
+    }
+
+    getLoginDialogBoxButton() {
+        return cy.get('button[type="submit"]');
+    }
+
+    getEmailTextBox() {
+        return cy.get("#email")
+    }
+
+    getNextButtonUnderEmail() {
+        return cy.get('button[type="button"]');
+    }
+
+    getPasswordTexBox() {
+        return cy.get('#password');
+    }
+
+    getLoginButtonUnderPassword() {
+        return cy.get('button[type="button"]');
     }
 }
