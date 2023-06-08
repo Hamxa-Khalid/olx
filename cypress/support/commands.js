0d01ee4f845098db4ccfd70ee0e5b3f04c3df8ae
Hamxa-Khalid <hamxi8381@gmail.com>
2023-06-09 00:29:05 +0500

    Added Login Code To Custom Command So it can be re useable plus create fixture file to use data

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,31 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { loginOlx } from "../support/pages/loginOlxPO/loginOlxPO";
+const loginOlxObj = new loginOlx();
 
 Cypress.Commands.add('closeAlertBox', (label) =>{
     cy.url().should('include', 'olx.com');
     cy.title().should('eq', 'OLX - Buy and Sell for free anywhere in Pakistan with OLX online classifieds');
     cy.get('button').contains(label).should('be.visible').click();
+})
+
+Cypress.Commands.add('loginOlxcmd', (email,password) => {
+
+   
+
+        loginOlxObj.getOlxLoginButton().click();
+        loginOlxObj.getLoginDialogBoxButton().contains("Continue with Email").click();
+        loginOlxObj.getEmailTextBox().type(email)
+        loginOlxObj.getNextButtonUnderEmail().contains("Next").click();
+        loginOlxObj.getPasswordTexBox().type(password);
+        loginOlxObj.getLoginButtonUnderPassword().contains("Log in").click();
+
 })
