355e4c81790eb53af40b0be922b0471cf5e9f701
Hamxa-Khalid <hamxi8381@gmail.com>
2023-06-13 19:48:09 +0500

    Put the category selection Code In command.js to make it reuseable

--- cypress/test/adCreationOlx/adCreationOlx.cy.js
+++ cypress/test/adCreationOlx/adCreationOlx.cy.js
@@ -11,11 +11,9 @@ describe("Create Ad On Olx", function () {
     })
 
     it("Create Olx Ad", function () {
+        let category = ['Services','Web Development']
         cy.loginOlxcmd(this.data.email, this.data.password);
-        adCreationOlxObj.getHeaderSellButton().click();
-        cy.url().should('include', '/post');
-        adCreationOlxObj.getAdCategoryOlx().contains('Services').click();
-        adCreationOlxObj.getAdCategoryOlx().contains('Web Development').click();
+        cy.selectCategorycmd(category);
         cy.url().should('include', '/post/attributes');
         adCreationOlxObj.getAdTitleTextboxOlx().type("QA Engineer");
         adCreationOlxObj.getAdDescriptionTexBox().type("As a top software QA company, our goal is to develop custom solutions that support your current goals and operational processes.");
